9791cd08fd3295e58cb7fe7250e9f24af63f96d5
nfortiz <nf.ortiz@uniandes.edu.co>
2020-07-14 22:52:01 -0500

    Now the app is responsive!!

--- App.tsx
+++ App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import marked from 'marked';
 
 import Header from './components/Header';
+import EditIcon from './assets/EditIcon';
+import EyeIcon from './assets/EyeIcon';
 
 const App: React.FunctionComponent = () => {
   const output = React.useRef(null);
+
+  const [fullView, setFullView] = React.useState(false)
   const [markdown, setMarkdown] = React.useState('# Hello world \n');
 
   React.useEffect(() => {
     function onResize(x){
       if(output.current && x.matches){
-
         console.log('The screen has <600px width');
-
       } else {
         console.log('The screen has >600px width');
       }
@@ -38,6 +40,13 @@ const App: React.FunctionComponent = () => {
   function onChange (e: React.ChangeEvent<HTMLTextAreaElement>) {
     setMarkdown(e.target.value)
   }
+
+  function onClick (): void {
+    setFullView(!fullView);
+  }
+
+  const Icon = fullView ? EditIcon : EyeIcon;
+
   return (
     <div><Header />
     <main className='content'>
@@ -45,9 +54,15 @@ const App: React.FunctionComponent = () => {
         className='user-input'
         value={markdown}
         onChange={onChange}></textarea>
-        <div 
+      <div 
         ref={output}
-        className='html-output'></div>
+        className={`html-output ${fullView && 'active-panel'}`}></div>
+      <div className='buttons'>
+        <Icon 
+          onClick={onClick}
+          styles='button' 
+        />
+      </div>
     </main>
     </div>
   );
